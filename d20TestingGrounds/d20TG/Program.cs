fd567f819a88ceb063d73abbbf3d04e45e2f7471
Emil Borowiec <emilborowiec@gmail.com>
2023-07-13 21:11:16 +0200

    layout of scenario setup page

--- d20TestingGrounds/d20TG/Program.cs
+++ d20TestingGrounds/d20TG/Program.cs
@@ -1,3 +1,6 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Blazored.LocalStorage;
 using d20TG;
 using d20TG.Features.ScenarioSetup.Services;
 using d20TG.Shared;
@@ -13,5 +16,7 @@ builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.
 builder.Services.AddScoped<ICombatScenarioService, CombatScenarioService>();
 builder.Services.AddScoped<ICombatScenarioRepository, CombatScenarioRepository>();
 builder.Services.AddSingleton<IMyNavigationManager, MyNavigationManager>();
+
+builder.Services.AddBlazoredLocalStorage();
 
 await builder.Build().RunAsync();
