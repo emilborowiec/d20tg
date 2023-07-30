40382481a2f3e1756f11eb5ea893fe7d36502e27
Emil Borowiec <emilborowiec@gmail.com>
2023-07-30 11:10:16 +0200

    in-place scenario name editing

---
+++ d20TestingGrounds/d20TG/Features/Scenarios/Model/ScenarioNameFormModel.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace d20TG.Features.Scenarios.Model;
+
+public class ScenarioNameFormModel
+{
+    [MinLength(1), MaxLength(100)]
+    public string Name { get; set; } = string.Empty;
+}
